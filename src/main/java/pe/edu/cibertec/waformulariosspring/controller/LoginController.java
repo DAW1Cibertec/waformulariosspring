0004f25c823ae595292550234aa4056cc84a127d
DAW1Cibertec <daw1_cibertec@hotmail.com>
2024-03-31 11:22:40 -0500

    modelo agregado

--- src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
@@ -1,13 +1,16 @@
 package pe.edu.cibertec.waformulariosspring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pe.edu.cibertec.waformulariosspring.model.LoginModel;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("loginModel", new LoginModel());
         return "login";
     }
 
--- src/main/java/pe/edu/cibertec/waformulariosspring/model/LoginModel.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/model/LoginModel.java
@@ -1,4 +1,11 @@
 package pe.edu.cibertec.waformulariosspring.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginModel {
+    private String usuario;
+    private String password;
 }
