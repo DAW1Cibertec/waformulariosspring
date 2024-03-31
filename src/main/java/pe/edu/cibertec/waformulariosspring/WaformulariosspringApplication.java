ffa214c2cda274b9e2930a6125c1be536744764a
DAW1Cibertec <daw1_cibertec@hotmail.com>
2024-03-31 11:09:00 -0500

    diseño de login terminado

---
+++ src/main/java/pe/edu/cibertec/waformulariosspring/WaformulariosspringApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.waformulariosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaformulariosspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaformulariosspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/waformulariosspring/WaformulariosspringApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.waformulariosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaformulariosspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
